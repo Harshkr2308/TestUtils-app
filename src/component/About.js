07f22953fd616e9d219a833060aed408ef8bd930
Harsh kumar <harshkr5247@gmail.com>
2024-01-30 10:44:35 +0530

    fixed

--- src/component/About.js
+++ src/component/About.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
-export default function About(props) {
+const About=(props)=> {
 
-    // const [myStyle, setMyStyle] = useState({
-    //     color: 'black',
-    //     backgroundColor: 'white'
-    // }) 
     let myStyle = {
         color: props.mode ==='dark'?'white':'#042743',
         backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
@@ -58,3 +54,4 @@ export default function About(props) {
         </div>
     )
 }
+export default About;
--- src/component/Alert.js
+++ src/component/Alert.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Alert(props) {
+const Alert=(props)=> {
     const capitalize = (word)=>{
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
